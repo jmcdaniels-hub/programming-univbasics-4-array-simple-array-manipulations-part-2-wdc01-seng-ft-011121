d9f72e33d69ca6e633484796c84bf3db486b2102
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-28 21:41:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,9 @@ def using_concat(my_favorite_things, more_favs)
   ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   all_my_favs = my_favorite_things.concat(more_favs)
-  
 end
+
+
+
 
 

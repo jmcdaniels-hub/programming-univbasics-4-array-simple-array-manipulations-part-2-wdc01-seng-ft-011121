605e8ccaea9e5a4a8b162c239751c1c35ac09fb8
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-28 22:16:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,6 +28,7 @@ def using_delete(instructors, string = "Steven")
 end
 
 
-def using_delete_at
-  
+def using_delete_at(famous_robots)
+  ["Johnny 5", "R2D2", "Robocop"]
+    deleted_robot = using_delete_at(famous_robots, 2)
 end 
